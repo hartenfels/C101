50e3af571dd6e62d9ea1ab8ce76dac1b4ba13b36
hartenfels <carsten.hartenfels@googlemail.com>
2014-04-01 04:56:26 +0200

    Documentation.

--- include/c101_Company.h
+++ include/c101_Company.h
@@ -5,6 +5,31 @@
 extern "C" {
 #endif
 
+/**<!--===============================================================================-->
+
+@file c101_Company.h Class for representing a hierarchical company.
+
+
+Feature
+=======
+
+Implements Feature:Hierarchical_company.
+
+
+How It Works
+============
+
+The implementation of the company is class-like, with a struct containing the name and
+a vector of departments and associated operations in this file.
+
+
+Usage
+=====
+
+Use ::c101_newCompany to construct and ::c101_freeCompany to destruct a company.
+
+<!--================================================================================-->*/
+
 struct c101_Company {
     char*              name;
     struct c101_Vector subunits;
@@ -15,7 +40,9 @@ struct c101_Company* c101_newCompany(char*  name,
                                      size_t subunitCount,
                                             ...);
 
+
 void                c101_freeCompany(struct c101_Company*);
+
 
 #ifdef __cplusplus
 }

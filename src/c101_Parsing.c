3e370195fb3ec978913971605ced28f308ce0d2a
hartenfels <carsten.hartenfels@googlemail.com>
2014-05-13 01:33:42 -0400

    Made parsing and unparsing symmetric.

--- src/c101_Parsing.c
+++ src/c101_Parsing.c
@@ -2,142 +2,93 @@
 #include "c101_Company.h"
 #include "c101_Subunit.h"
 #include "c101_Util.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "c101_Vector.h"
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 
-static void
-c101_warnExcessive(lua_State* lua, int idx)
-{
-    if (idx < 0)
-        --idx;
-    lua_pushnil(lua);
-    while (lua_next(lua, idx)) {
-        lua_pop(lua, 1);
-        fputs("Warning: excessive table key \"", stderr);
-        fputs(lua_tostring(lua, -1),             stderr);
-        fputs("\" ignored\n",                    stderr);
-    }
-}
+struct ParseData {
+    struct c101_Company* company;
+    struct c101_Vector   stack;
+};
 
-static void
-c101_popField(lua_State* lua, int idx, const char* key)
-{
-    if (!lua_checkstack(lua, 2))
-        c101_luaError(NULL, "Could not enlarge stack");
-    lua_getfield(lua, idx, key);
-    lua_pushnil(lua);
-    lua_setfield(lua, idx < 0 ? idx - 2 : idx, key);
-}
-
-static void
-c101_popFields(lua_State* lua, ...)
-{
-    if (!lua_istable(lua, -1))
-        c101_luaError(lua, "Cannot pop fields from something not a table");
-
-    va_list vl;
-    va_start(vl, lua);
-    int idx = -1;
-    const char* key;
-    while ((key = va_arg(vl, const char*)))
-        c101_popField(lua, idx--, key);
-    c101_warnExcessive(lua, idx);
-    lua_remove(lua, idx);
-}
-
-
-static struct c101_Subunit*
-c101_parseEmployee(lua_State* lua)
-{
-    c101_popFields(lua, "salary", "address", "name", NULL);
-
-    if (!lua_isstring(lua, -1))
-        c101_luaError(lua, "Missing field \"name\" in employee");
-    if (!lua_isstring(lua, -2))
-        c101_luaError(lua, "Missing field \"address\" in employee");
-    if (!lua_isnumber(lua, -3))
-        c101_luaError(lua, "Missing field \"salary\" in employee");
-
-    struct c101_Subunit* subunit = c101_newEmployee(lua_tostring(lua, -1),
-                                                    lua_tostring(lua, -2),
-                                                    lua_tonumber(lua, -3));
-    lua_pop(lua, 3);
-    return subunit;
-}
-
-static void
-c101_parseEmployees(lua_State* lua, struct c101_Department* parent)
-{
-    lua_pushnil(lua);
-    while (lua_next(lua, -2))
-        c101_pushBack(&parent->subunits, c101_parseEmployee(lua));
-}
-
-static void c101_parseSubDepartments(lua_State* lua, struct c101_Department* parent);
-
-static struct c101_Subunit*
-c101_parseDepartment(lua_State* lua)
-{
-    c101_popFields(lua, "departments", "employees", "name", NULL);
-
-    if (!lua_isstring(lua, -1))
-        c101_luaError(lua, "Missing field \"name\" in department");
-    struct c101_Subunit* subunit = c101_newDepartment(lua_tostring(lua, -1), 0);
-    lua_pop(lua, 1);
-
-    if (!lua_isnil(lua, -1))
-        c101_parseEmployees(lua, &subunit->department);
-    lua_pop(lua, 1);
-
-    if (!lua_isnil(lua, -1))
-        c101_parseSubDepartments(lua, &subunit->department);
-    lua_pop(lua, 1);
-
-    return subunit;
-}
-
-static void
-c101_parseDepartments(lua_State* lua, struct c101_Company* company)
-{
-    lua_pushnil(lua);
-    while (lua_next(lua, -2))
-        c101_pushBack(&company->subunits, c101_parseDepartment(lua));
-}
-
-static void
-c101_parseSubDepartments(lua_State* lua, struct c101_Department* parent)
-{
-    lua_pushnil(lua);
-    while (lua_next(lua, -2))
-        c101_pushBack(&parent->subunits, c101_parseDepartment(lua));
-}
-
-static struct c101_Company*
+static int
 c101_parseCompany(lua_State* lua)
 {
-    c101_popFields(lua, "departments", "name", NULL);
+    struct ParseData* data = lua_touserdata (lua, 1);
+    const char      * name = lua_tostring   (lua, 2);
 
-    if (!lua_isstring(lua, -1))
-        c101_luaError(lua, "Missing field \"name\" in company");
-    struct c101_Company* company = c101_newCompany(lua_tostring(lua, -1), 0);
-    lua_pop(lua, 1);
+    data->company = c101_newCompany(name, 0);
+    data->stack   = c101_newVector (1, NULL);
 
-    if (!lua_isnil(lua, -1))
-        c101_parseDepartments(lua, company);
-    lua_pop(lua, 1);
+    return 0;
+}
 
-    return company;
+static int
+c101_parseDepartment(lua_State* lua)
+{
+    struct ParseData* data = lua_touserdata (lua, 1);
+    const char      * name = lua_tostring   (lua, 2);
+
+    struct c101_Subunit* subunit = c101_newDepartment(name, 0);
+    struct c101_Vector * parent  = data->stack.size
+            ? &((struct c101_Subunit*) c101_back(&data->stack))->department.subunits
+            : &data->company->subunits;
+    c101_pushBack(parent,       subunit);
+    c101_pushBack(&data->stack, subunit);
+
+    return 0;
+}
+
+static int
+c101_parseEmployee(lua_State* lua)
+{
+    struct ParseData* data    = lua_touserdata (lua, 1);
+    const char      * name    = lua_tostring   (lua, 2);
+    const char      * address = lua_tostring   (lua, 3);
+    double            salary  = lua_tonumber   (lua, 4);
+
+    struct c101_Subunit* subunit = c101_newEmployee(name, address, salary);
+    struct c101_Vector * parent  =
+              &((struct c101_Subunit*) c101_back(&data->stack))->department.subunits;
+    c101_pushBack(parent, subunit);
+
+    return 0;
+}
+
+static int
+c101_endDepartment(lua_State* lua)
+{
+    struct ParseData* data = lua_touserdata(lua, 1);
+    c101_popBack(&data->stack);
+    return 0;
 }
 
 struct c101_Company*
 c101_parse(const char* file)
 {
-    lua_State          * lua     = c101_initLua		(file);
-	struct c101_Company* company = c101_parseCompany(lua );
+    lua_State* lua = c101_initLua("parsing.lua");
+
+    lua_pushcfunction(lua,  c101_parseCompany    );
+    lua_setglobal    (lua, "c101_parseCompany"   );
+    lua_pushcfunction(lua,  c101_parseDepartment );
+    lua_setglobal    (lua, "c101_parseDepartment");
+    lua_pushcfunction(lua,  c101_parseEmployee   );
+    lua_setglobal    (lua, "c101_parseEmployee"  );
+    lua_pushcfunction(lua,  c101_endDepartment   );
+    lua_setglobal    (lua, "c101_endDepartment"  );
+
+    lua_getglobal(lua, "parseCompany");
+    struct ParseData data;
+    lua_pushlightuserdata(lua, &data);
+    if (luaL_dofile(lua, file))
+        c101_luaError(lua, lua_tostring(lua, -1));
+
+    if (lua_pcall(lua, 2, 0, 0))
+        c101_luaError(lua, lua_tostring(lua, -1));
     lua_close(lua);
-	return company;
+
+    c101_freeVector(&data.stack);
+	return data.company;
 }
 
